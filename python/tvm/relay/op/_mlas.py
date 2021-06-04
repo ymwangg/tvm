e8b4bbd4a4c4de59a4a894ce4b9ccf17d7b4af44
Yanming Wang <yanmingwang01@gmail.com>
2021-06-04 19:17:57 +0000

    Add ASF header

--- python/tvm/relay/op/_mlas.py
+++ python/tvm/relay/op/_mlas.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Strategy and AlterOpLayout functions of MLAS operators"""
 from . import op as reg
 from tvm.target import override_native_generic_func, Target, generic_func
 from tvm.topi.nn import dense_alter_layout
